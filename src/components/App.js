37cc543c36ae44c4aa3abdeb24bdb54acefc0e7a
Fabien Morival <fabienmorival@gmail.com>
2024-05-05 14:42:55 +0200

    Serveur local

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ import Library from '../services/Library';
 import io from 'socket.io-client';
 import sorter from '../utility/CollectionSorter';
 
-const serverURL = /*process.env.SERVER_URL ||*/ 'http://localhost:8080'/* || 'https://sensuba.herokuapp.com/'*/;
+const serverURL = process.env.SERVER_URL || 'http://localhost:8080'/* || 'https://sensuba.herokuapp.com/'*/;
 
 const nocards = 1150;
 
