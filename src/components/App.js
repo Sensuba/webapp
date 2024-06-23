e113695b3d7b04f7b96ebc66883fa2eadf55f952
Fabien Morival <fabienmorival@gmail.com>
2024-06-23 11:46:04 +0200

    tmp serv

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ import Library from '../services/Library';
 import io from 'socket.io-client';
 import sorter from '../utility/CollectionSorter';
 
-const serverURL = 'https://877b4e02e434f8.lhr.life'/* || 'https://sensuba.herokuapp.com/'*/;
+const serverURL = 'https://40a5a5a4464813.lhr.life'/* || 'https://sensuba.herokuapp.com/'*/;
 
 const nocards = 1150;
 
