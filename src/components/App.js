c630ca87022494b4a26d8a1b69f759faa3d002b5
NeiNalen <neinalen0@gmail.com>
2024-10-22 23:27:44 +0200

    change server

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ import Library from '../services/Library';
 import io from 'socket.io-client';
 import sorter from '../utility/CollectionSorter';
 
-const serverURL = 'https://e850023c8cb5c7.lhr.life'/* || 'https://sensuba.herokuapp.com/'*/;
+const serverURL = 'http://4.233.219.185:8080'/* || 'https://sensuba.herokuapp.com/'*/;
 
 const nocards = 1150;
 
