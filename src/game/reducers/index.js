3cd294680f8a24bc1c2fe8ba7c39a4505c2ca68c
NeiNalen <neinalen0@gmail.com>
2024-11-02 18:01:24 +0100

    Card changes

--- src/game/reducers/index.js
+++ src/game/reducers/index.js
@@ -2,7 +2,7 @@ import GameBoard from '../model/board/GameBoard';
 
 export default (state = new GameBoard(), n) => {
 
-  if (n.state) {
+  if (n.state) {console.log(n)
     state = n.state;
     state.notify(n.type, n.src, n.data);
   }
